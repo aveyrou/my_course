fa8847d08113f524e5310183df5f4c1f3491b500
Magomed Misrikhanov <misrikhanov.magomed@inbox.ru>
2026-06-04 14:45:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using TouristRoutes.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var dbPath = Path.Combine(builder.Environment.ContentRootPath, "tourist_routes.db");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite($"Data Source={dbPath}"));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    DbSeeder.Seed(db);
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
